a27675b908c3a31720238f170a0d5b2055439ff3
jmrapp1 <jmrapp1270@gmail.com>
2018-01-31 20:36:29 -0500

    added links

--- client/src/views/pages/home/HomePage.jsx
+++ client/src/views/pages/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './HomePage.css';
 import Container from '../../common/components/containers/Container';
+import { Link } from 'react-router-dom';
 
 class HomePage extends Component {
     render() {
@@ -8,6 +9,7 @@ class HomePage extends Component {
             <div className="app container-fluid">
                 <Container>
                     <h1>This is the Home Page</h1>
+                    <h1><Link to="/login">Login</Link></h1>
                 </Container>
             </div>
 
--- client/src/views/pages/login/LoginPage.jsx
+++ client/src/views/pages/login/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './LoginPage.css';
 import Container from '../../common/components/containers/Container';
+import { Link } from 'react-router-dom';
 
 class LoginPage extends Component {
     render() {
@@ -8,6 +9,7 @@ class LoginPage extends Component {
             <div className="app container-fluid">
                 <Container>
                     <h1>This is the Login</h1>
+                    <h1><Link to="/">Home</Link></h1>
                 </Container>
             </div>
 
