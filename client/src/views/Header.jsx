d48f93518e96460fca551feb3a444a2b08b2ed4a
jmrapp1 <jmrapp1270@gmail.com>
2018-01-31 20:42:15 -0500

    changing headers

--- client/src/views/Header.jsx
+++ client/src/views/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Header.css';
+import { Route, Switch } from 'react-router-dom';
 
 
 class Header extends Component {
@@ -7,7 +8,10 @@ class Header extends Component {
     render() {
         return (
             <div className="container-fluid">
-                <h1>Header</h1>
+                <Switch>
+                    <Route exact path="/login" component={ () => <h1>Login Page Header</h1> }/>
+                    <Route path="/*" component={ () => <h1>Home Page Header</h1> }/>
+                </Switch>
             </div>
         );
     }
