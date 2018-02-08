d749cc26a42001a13aa0653781f0c1d1ea058b7d
yuyya <mjrivera803@gmail.com>
2018-02-07 20:34:07 -0500

    added navbar

--- client/src/views/Header.jsx
+++ client/src/views/Header.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import './Header.css';
 import { Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const image = require('./common/image/icon.png');
 
 
 class Header extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <Switch>
-                    <Route exact path="/login" component={ () => <h1></h1> }/>
-                    <Route path="/*" component={ () => <h1></h1> }/>
-                </Switch>
+            <div id="header">
+                <div className="container-fluid">
+                    <div className="col-sm-4 col-md-4 col-md-offset-4 text-center" id="image-header">
+                        <nav className="navbar navbar-default">
+                            <div className="container-fluid">
+                                <ul className="nav navbar-nav">
+                                    <li><Link to="/login">Login</Link></li>
+                                    <li><Link to="/signup">Sign Up</Link></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
             </div>
         );
     }
