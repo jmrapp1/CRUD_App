9d5015bf4b49316c2ef5757c11a4e3b3e9570482
Yuyya <mjrivera803@gmail.com>
2018-02-13 15:17:36 -0500

    added welcome message

--- client/src/views/pages/index/customer/Customer.jsx
+++ client/src/views/pages/index/customer/Customer.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './Customer.css';
 import Container from '../../../common/components/containers/Container';
 import { Link } from 'react-router-dom';
+import { Actions as UserActions } from '../../../../redux/modules/user';
+import { Actions as AlertActions } from '../../../../redux/modules/alert';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 class Customer extends Component {
     render() {
         return (
             <div className="app container-fluid">
                 <Container>
-                    <h1>You Have Signed In As A Customer</h1>
+                    <h1>Welcome {this.props.user.firstName} {this.props.user.lastName}</h1>
                     <h1><Link to="/login">Return to Login</Link></h1>
                 </Container>
             </div>
@@ -17,4 +23,16 @@ class Customer extends Component {
     }
 }
 
-export default Customer;
+Customer.propTypes = {
+    user: PropTypes.object,
+};
+
+export default connect(
+    store => ( {
+        user: store.user.user
+    } ),
+    dispatch => ( {
+        login: bindActionCreators(UserActions.login, dispatch),
+        clearAlert: bindActionCreators(AlertActions.clear, dispatch)
+    } )
+)(Customer);
