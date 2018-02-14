9871365abfad7a4936fb24c2f5052a27567ace02
Yuyya <mjrivera803@gmail.com>
2018-02-14 17:19:29 -0500

    added simple footer

--- client/src/views/layout/Footer.jsx
+++ client/src/views/layout/Footer.jsx
@@ -6,7 +6,8 @@ class Footer extends Component {
     render() {
         return (
             <div id="footer" className="">
-
+                <p className="copy text-center">&copy; Very Professional Footer  Created by Jon Rapp and Mike Rivera</p>
+                <p className="creator text-center">Created by Jon Rapp and Mike Rivera</p>
             </div>
 
         );
