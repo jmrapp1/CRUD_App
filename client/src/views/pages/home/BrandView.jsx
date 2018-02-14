d3e1a87f57ccfdbe143070972fd904da83f20884
jmrapp1 <jmrapp1270@gmail.com>
2018-02-14 13:10:38 -0500

    hide clouds on mobile

--- client/src/views/pages/home/BrandView.jsx
+++ client/src/views/pages/home/BrandView.jsx
@@ -3,25 +3,29 @@ import { Route } from "react-router-dom";
 import { NavItem } from "react-bootstrap";
 import * as $ from 'jquery';
 import './BrandView.css';
-import Cloud from '../../common/images/cloud.jpg';
+import Cloud from '../../common/images/cloud.png';
 
 class BrandView extends Component {
 
     componentDidMount() {
         $('.logo-header').delay(200).animate({ opacity: 1 }, 700);
         $(".cloud-left").animate({
-            marginLeft: "16%"
+            marginLeft: "16%",
+            zIndex: 0
         }, 1000);
         $(".cloud-right").animate({
-            marginLeft: "77%"
+            marginLeft: "77%",
+            zIndex: 0
         }, 1000);
     }
 
     render() {
         return (
             <div id="brand">
-                <img className="cloud cloud-left" src={ Cloud } />
-                <img className="cloud cloud-right" src={ Cloud } />
+                <div id="clouds" className="hidden-sm">
+                    <img className="cloud cloud-left" src={ Cloud }/>
+                    <img className="cloud cloud-right" src={ Cloud }/>
+                </div>
                 <div className="logo-header text-center">
                     <h1>UManage</h1>
                     <h2>Power your business with ease and reliability</h2>
@@ -31,4 +35,5 @@ class BrandView extends Component {
     }
 
 }
+
 export default BrandView;
