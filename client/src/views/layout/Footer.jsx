96edf9ec43e9c4114b99d699c80ebd4b9ae94ee3
Yuyya <mjrivera803@gmail.com>
2018-02-14 17:41:37 -0500

    footer remove until bug is fixed

--- client/src/views/layout/Footer.jsx
+++ client/src/views/layout/Footer.jsx
@@ -5,9 +5,9 @@ import './Footer.css';
 class Footer extends Component {
     render() {
         return (
+
             <div id="footer" className="">
-                <p className="copy text-center">&copy; Very Professional Footer  Created by Jon Rapp and Mike Rivera</p>
-                <p className="creator text-center">Created by Jon Rapp and Mike Rivera</p>
+          
             </div>
 
         );
