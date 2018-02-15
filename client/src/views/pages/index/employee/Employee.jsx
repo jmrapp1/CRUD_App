5ac0779a3fc49bd25a08d62943cb7619e322435d
yuyya <mjrivera803@gmail.com>
2018-02-14 19:40:41 -0500

    employee page updated

--- client/src/views/pages/index/employee/Employee.jsx
+++ client/src/views/pages/index/employee/Employee.jsx
@@ -8,10 +8,18 @@ class Employee extends Component {
     render() {
         return (
             <div id="employee-home">
-                <div className="col-sm-12 col-md-8 col-md-offset-2 vertical-center">
-                    <Container className="container">
-                        <h1>Welcome { this.props.user.firstName } { this.props.user.lastName }. You're an employee!</h1>
-                    </Container>
+                <div className="row">
+                    <div className="info col-sm-12 col-md-4 col-md-offset-6 text-right">
+                        <Container className="container col-md-4 .ml-auto">
+                            <u1>
+                                    <p>Fist Name: {this.props.user.firstName}</p>
+                                    <p>Last Name: {this.props.user.lastName}</p>
+                                    <p>Email: {this.props.user.email}</p>
+                                    <p>Rate: $24.25</p>
+
+                            </u1>
+                        </Container>
+                    </div>
                 </div>
             </div>
         );
